99ca574d64fdcaa97e2a1149a7b825d174ede5e8
Lucas Guillerot lguiller <lguiller@student.42.fr>
2022-01-04 18:18:58 +0100

    suppression des espace dans l'affichage de l'heure

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const timestampToTimeFormat = (timestamp) => {
 	const duration = new Date(timestamp ? timestamp : 0);
 	const splitDuration = duration.toISOString().substr(11, 8).split(':');
-	let strDuration = `${splitDuration[0]} h ${splitDuration[1]} mn ${splitDuration[2]} sec`;
+	let strDuration = `${splitDuration[0]}h ${splitDuration[1]}mn ${splitDuration[2]}sec`;
 	return (strDuration);
 };
-
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
 	client.users.fetch(newMember.id).then(user => {
