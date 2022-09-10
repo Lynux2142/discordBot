c92fd738934807bc26ad2085118c3de98a8b8276
Lucas Guillerot lguiller <lguiller@student.42.fr>
2022-09-10 17:55:21 +0200

    delete all write a possibility to !time everywhere

--- LynuxBot.py
+++ LynuxBot.py
@@ -12,7 +12,6 @@ userInfo = {}
 
 @client.event
 async def on_ready():
-	sys.stdout.write(f'We have logged in as {client.user}')
 	now = datetime.now().replace(microsecond = 0)
 	for guild in client.guilds:
 		for channel in guild.voice_channels:
@@ -33,25 +32,20 @@ async def on_voice_state_update(member, before, after):
 	else:
 		message = f'[{now.strftime("%d/%m/%Y, %H:%M:%S")}] INFO: {member} has left {before.channel.name} to join {after.channel.name}\n'
 	await channelLog.send(message)
-	sys.stdout.write(message)
 
 @client.event
 async def on_message(message):
-	if (message.channel.id == int(os.getenv('CHANNEL_LOG_ID'))):
-		if (message.content.startswith('!time')):
-			now = datetime.now().replace(microsecond = 0)
-			if (message.mentions):
-				reply_message = ""
-				for member in message.mentions:
-					if member.id in userInfo:
-						reply_message += f'{member.name}#{member.discriminator} is connected since {now - userInfo[member.id]}\n'
-						continue
-					reply_message += f'{member.name}#{member.discriminator} is not connected to the server\n'
-				await message.reply(reply_message, mention_author = False)
-				sys.stdout.write(f'[{now.strftime("%d/%m/%Y, %H:%M:%S")}] INFO: {message.author.name} asked for some members time\n')
-			else:
-				reply_message = now - userInfo[message.author.id]
-				await message.reply(reply_message, mention_author = False)
-				sys.stdout.write(f'[{now.strftime("%d/%m/%Y, %H:%M:%S")}] INFO: {message.author.name} asked for his time\n')
+	if (message.content.startswith('!time')):
+		now = datetime.now().replace(microsecond = 0)
+		if (message.mentions):
+			reply_message = ""
+			for member in message.mentions:
+				if member.id in userInfo:
+					reply_message += f'{member.name}#{member.discriminator} is connected since {now - userInfo[member.id]}\n'
+					continue
+				reply_message += f'{member.name}#{member.discriminator} is not connected to the server\n'
+			await message.reply(reply_message, mention_author = False)
+			return
+		await message.reply(now - userInfo[message.author.id], mention_author = False)
 
 client.run(os.getenv('BOT_TOKEN'))
